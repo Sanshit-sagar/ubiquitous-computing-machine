edd75af44e6234cc638a165f64e53631437fcac3
Sanshit Sagar <sasagar@ucsd.edu>
2021-06-17 02:33:11 -0700

    updating google keys again

--- .history/pages/api/auth/[...nextauth]_20210617015202.js
+++ .history/pages/api/auth/[...nextauth]_20210617015202.js
@@ -2,15 +2,17 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
+  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
     Providers.Google({
-      clientId: '861198350688-kssro6egps13h3skj93ip0hrehg3qkl4.apps.googleusercontent.com',
-      clientSecret: 's3PW9rbLIBzdwzAJ8f66ww3q',
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
+    // ...add more providers here
   ],
 });
--- .history/pages/api/auth/[...nextauth]_20210617023239.js
+++ .history/pages/api/auth/[...nextauth]_20210617023239.js
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
+  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
