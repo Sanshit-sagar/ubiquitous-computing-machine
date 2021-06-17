3aa95a34e021f5f472ffee2cf036fc36f2fef6f9
Sanshit Sagar <sasagar@ucsd.edu>
2021-06-17 00:41:56 -0700

    removing fetcher as default export from utils

--- .history/components/MarkdownEditor/InputForm_20210617004045.js
+++ .history/components/MarkdownEditor/InputForm_20210617004045.js
@@ -1,7 +1,7 @@
 
 import React, { useContext } from 'react';
 import useSWR from 'swr'
-import { fetcher } from '../../lib/utils'
+import fetcher from '../../lib/utils'
 import toast from 'react-hot-toast';
 import { useSession } from 'next-auth/client';
 import { GlobalStore } from '../../store';
