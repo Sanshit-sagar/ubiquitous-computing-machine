46f287a29dd7a12c7cf86336e0a54656ca752efa
Sanshit Sagar <sasagar@ucsd.edu>
2021-07-05 14:46:57 -0700

    removing footer

---
+++ .history/sections/Footer_20210705144628.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SubscriptionCard from '../components/SubscriptionCard'
+
+const Footer = () => (
+    <footer className="w-80 px-2 mt-5 py-2 bg-black">
+        <SubscriptionCard /> 
+    </footer>
+);
+  
+export default Footer;
