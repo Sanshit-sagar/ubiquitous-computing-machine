34fe15eef71c70dcc046eaa57ecd11f71d98d749
Sanshit Sagar <sasagar@ucsd.edu>
2021-07-05 14:30:59 -0700

    removing react-timeseries-charts as a dep

--- .history/components/Toolbar/TimeControl_20210626133136.js
+++ .history/components/Toolbar/TimeControl_20210626133136.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Button } from '@supabase/ui'
+import { Button, Windmill } from '@windmill/react-ui'
 
 const options = [
     { id: 0, timeControl: 1, title: '1 Day', key: '1'},
