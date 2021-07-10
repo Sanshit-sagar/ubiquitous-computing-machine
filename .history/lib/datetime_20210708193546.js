e7997fc633a7ef721667227389950ea6686ddbdd
Sanshit Sagar <sasagar@ucsd.edu>
2021-07-10 09:11:50 -0700

    removing the separate SideMenu.js class

--- .history/lib/datetime_20210708193546.js
+++
@@ -1,8 +0,0 @@
-
-export function getLocaleTimestring(timestamp) {
-    return new Date(timestamp).toLocaleTimeString()
-}
-
-export function getDateString(timestamp) {
-    return new Date(timestamp).toDateString()
-}
--- .history/pages/api/v1/day_20210708093725.js
+++
@@ -1,7 +0,0 @@
-export default function (req, res) {
-    return new Promise(function (resolve) {
-      res.send(req.body.text);
-      resolve();
-    });
-}
-  
